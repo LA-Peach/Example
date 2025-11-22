ddc1d4c39308d46d919c31ee8c4648c660b5e20e
LA-Peach <leaannwillis@student.purdueglobal.edu>
2025-11-22 10:36:22 -0500

    Auto-update ngrok API URL

--- src/api.js
+++ src/api.js
@@ -1,9 +1,3 @@
-import axios from 'axios'
-
-// Replace this with your ngrok HTTPS URL, e.g.:
-// const BASE_URL = 'https://abcd1234.ngrok-free.app/api/products'
-const BASE_URL = 'unsufferably-exceedable-greta.ngrok-free.dev'
-
-export const API = axios.create({
-  baseURL: BASE_URL,
-})
+import axios from 'axios' 
+const BASE_URL = '[{name/api/products' 
+export const API = axios.create({ baseURL: BASE_URL }) 
